b1d5090461f5ea42aa2b15174ff5b152bdeea525
magdapoppins <mmbste@utu.fi>
2018-04-22 12:19:42 +0300

    Changed stuff...

--- DAY_2/Adventure/App.js
+++ DAY_2/Adventure/App.js
@@ -9,29 +9,77 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  AsyncStorage,
+  Button,
+  Alert
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { getBatteryLevel } from 'react-native-device-info';
+import RNFS from 'react-native-fs';
 
 type Props = {};
 export default class App extends Component<Props> {
+constructor(props) {
+  super(props);
+  this.state = {name: '', battery: '', files: []}
+}
+
+componentDidMount(){
+
+  RNFS.readDir(RNFS.MainBundlePath) // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
+  .then((result) => {
+    console.warn('GOT RESULT', result);
+    this.setState({files: result});
+    console.warn('FILES IS NOW ', this.state.files)
+    // stat the first file
+    return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+  })
+  .then((statResult) => {
+    if (statResult[0].isFile()) {
+      // if we have a file, read it
+      return RNFS.readFile(statResult[1], 'utf8');
+    }
+
+    return 'no file';
+  })
+  .then((contents) => {
+    // log the file contents
+    console.log(contents);
+  })
+  .catch((err) => {
+    console.log(err.message, err.code);
+  });
+
+  AsyncStorage.getItem('myName').then(name => {
+    this.setState({name: name});
+  })
+
+  // setInterval(() => {
+  //   getBatteryLevel().then(num => {
+  //     this.setState()
+  //   })
+  // }, 1000)
+}
+
+onPress = () => {
+  AsyncStorage.setItem('myName', 'Clicker');
+
+  AsyncStorage.multiSet([
+    ['myName', 'Magda'],
+    ['myAge', '21'],
+    ['myFood', 'pizza']
+  ])
+}
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Adventure!
         </Text>
+        <Button title="Click me." onPress={Alert.alert("Hi!")} />
         <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        {RNFS.DocumentDirectoryPath}
         </Text>
       </View>
     );
@@ -43,16 +91,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'purple',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
+    color: 'white'
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: 'yellow',
     marginBottom: 5,
+    fontSize: 20
   },
 });
